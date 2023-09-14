99266624d0aa75583616c69cb75f5e7c20a7135d
Admin <Admin@DESKTOP-LI4EBVJ>
2023-09-14 14:50:21 +0530

    DATA TYPES

---
+++ src/DatatypesDemo/datatype.java
@@ -0,0 +1,27 @@
+package DatatypesDemo;
+
+public class datatype {
+	public static void main(String[] args) {
+		byte a = 123;
+		System.out.println(a);
+		short b = 1234;
+		System.out.println(b);
+		int c = 12345678;
+		System.out.println(c);
+		long d = 1234567891;
+		System.out.println(d);
+		char e = 'A';
+		System.out.println(e);
+		float f = 58.123f;
+		System.out.println(f);
+		double g = 99.999999;
+		System.out.println(g);
+		boolean h = false;
+		System.out.println(h);
+		
+		String s = "hello world";
+		System.out.println(s);
+		
+	}
+
+}
